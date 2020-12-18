6c8bef99c3fe0ba8b4ad7caa1067fcaa54e5eaa6
louis80 <56889786+louis80@users.noreply.github.com>
2020-12-18 18:28:53 +0100

    Updates

--- src/components/python/database.js
+++
@@ -1,5 +0,0 @@
-function PythonDatabase() {
-    return <div style={{border:'solid green 10px', height:'100%', width:'100%'}}> PythonDatabase </div>;
-}
-
-export default PythonDatabase;
--- src/components/python/dataframe.js
+++
@@ -1,5 +0,0 @@
-function PythonDataframe() {
-    return <div style={{border:'solid green 10px', height:'100%', width:'100%'}}> PythonDataframe </div>;
-}
-
-export default PythonDataframe;
