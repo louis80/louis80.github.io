c059f97a739f944c0210f4315b90daf1ea279012
louis80 <56889786+louis80@users.noreply.github.com>
2021-01-19 14:57:55 +0100

    add React GA

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import ReactGA from 'react-ga';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('G-VYJT4TRYGN'); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
