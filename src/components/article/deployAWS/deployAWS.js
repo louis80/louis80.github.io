956f5d88bff425e4b225b3ccc193c82086e43a74
louis80 <56889786+louis80@users.noreply.github.com>
2021-04-08 11:19:41 +0200

    add create db

--- src/components/article/deployAWS/deployAWS.js
+++ src/components/article/deployAWS/deployAWS.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Highlight, { defaultProps } from "prism-react-renderer";
+import dracula from 'prism-react-renderer/themes/dracula';
 
 import { Link } from "react-router-dom";
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
@@ -6,6 +8,47 @@ import Typography from '@material-ui/core/Typography';
 import HomeIcon from '@material-ui/icons/Home';
 
 import img_recap from './img/recap_steps.PNG';
+import security_group_11 from './img/11_security_group.PNG';
+import security_group_12 from './img/12_security_group.PNG';
+import database_13 from './img/13_database.PNG';
+
+function CodeHighlight(codesample) {
+    return (
+    <Highlight theme={dracula} {...defaultProps} code={codesample} language="python">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+                ))}
+            </div>
+            ))}
+        </pre>
+        )}
+    </Highlight>
+    );
+}
+
+const ConnectionToDatabase = `
+import yaml
+from sqlalchemy import create_engine
+import psycopg2
+
+# need improvment
+try:
+    f = open('config.yml', 'r')
+    conf = yaml.safe_load(f)
+except:
+    conf = yaml.load(os.getenv("PROJECT_CONFIG"))
+
+engine = create_engine('postgresql://'
+      + conf["project-database"]["user"] + ':'
+      + conf["project-database"]["password"] + '@'
+      + conf["project-database"]["hostname"] +':' + '5432/'
+      + conf["project-database"]["name"])
+`;
+
 
 class DeployAWS extends React.Component {
     constructor(props) {
@@ -67,9 +110,66 @@ class DeployAWS extends React.Component {
 
 
             <h2 className='sub-title' style={{marginTop:'3rem'}}> I. Setup the database </h2>
-            <p className="text" style={{marginTop:'2rem'}}>
-            Text descriptin
+            <p className="text" style={{fontWeight:'bold', marginTop:'2rem'}}>
+            Create a security groups
             </p>
+            <p className="text" style={{marginTop:'1rem'}}>
+            In order to access the database from our API or local network if needed,
+            it is necessary to create security groups.
+            Those will enable us to manage network access to our Amazon RDS instance.<br/>
+            To create a security service, go to the EC2 dashboard and click on “Security groups” :
+            </p>
+
+            <img src={security_group_11} class="card-img img-article h-100" alt="image-article"></img>
+
+            <p className="text" style={{marginTop:'1rem'}}>
+            Then click on “Create security group”, fill the security group name and the description.<br/><br/>
+            Let’s add an inbound rules : select “All traffic” for the type and “Anywhere” for the source :
+            </p>
+
+            <img src={security_group_12} style={{border:'solid silver 0.5px'}} class="card-img img-article h-100" alt="image-article"></img>
+
+            <p className="text" style={{marginTop:'1rem'}}>
+            blabla security <br/><br/>
+            Finally click on “Create security group” at the bottom of the page
+            </p>
+
+            <p className="text" style={{fontWeight:'bold', marginTop:'2rem'}}>
+            Create the database
+            </p>
+            <p className="text" style={{marginTop:'1rem'}}>
+            Now that we have created the security group, let’s create the database.
+            To do so, go to RDS services and click on “Create database”.
+            There, you will find several settings section to be completed as follows
+            (those following settings are for our simple example, you can obviously adapt the setting according to your needs) :<br/>
+            <span style={{fontWeight:'540'}}>• Choose a database creation method </span> → Standard create <br/>
+            <span style={{fontWeight:'540'}}>• Engine options</span>  → PostgreSQL <br/>
+            <span style={{fontWeight:'540'}}>• Templates</span>  → Free tier <br/>
+            <span style={{fontWeight:'540'}}>• Settings</span>  → choose a DB instance name, master username and password and note them somewhere <br/>
+            <span style={{fontWeight:'540'}}>• DB instance class</span>  →  left as it is <br/>
+            <span style={{fontWeight:'540'}}>• Storage</span>  →  left as it is <br/>
+            <span style={{fontWeight:'540'}}>• Availability & durability</span> →  left as it is <br/>
+            <span style={{fontWeight:'540'}}>• Connectivity </span> →  Select "Yes" for public access and choose your security group previously created in the "Existing VPC security groups" section (named "rds-demo" in this example) :<br/><br/>
+
+            <img src={database_13} className='img-article' style={{textAlign:'center', margin:'auto', display:'flex'}} alt="image-article"></img>
+              <br/><br/>
+
+            <span style={{fontWeight:'540'}}>• Database authentication</span> →  left as it is <br/>
+            <span style={{fontWeight:'540'}}>• Additional configuration</span> →  left as it is <br/> <br/>
+            Finally click on “Create database” at the bottom of the page
+            </p>
+
+            <p className="text" style={{fontWeight:'bold', marginTop:'2rem'}}>
+            Access the database from Python
+            </p>
+
+            <div style={{marginTop:'15px'}}> {CodeHighlight(ConnectionToDatabase)} </div>
+
+            <p className="text" style={{marginTop:'1rem'}}>
+            test
+            </p>
+
+
 
             <h2 className='sub-title' style={{marginTop:'3rem'}}> II. Deploy the API </h2>
             <p className="text" style={{marginTop:'2rem'}}>
